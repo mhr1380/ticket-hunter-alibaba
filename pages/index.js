928ed72bacd33e93bcc370a3634e0735b01de7dd
mhr1380 <mhr.1380.2001@gmail.com>
2023-06-07 01:19:21 +0430

    first and last commit!

--- pages/index.js
+++ pages/index.js
@@ -1,124 +1,192 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import axios from "axios";
+import { useEffect, useReducer, useRef, useState } from "react";
+import { toast } from "react-toastify";
 
-const inter = Inter({ subsets: ['latin'] })
-
+const genereteProperText = (destination, date) => {
+  if (destination === "tehran") {
+    return `{"From":161,"To":1,"DepartureDate":"${new Date(date)
+      .toISOString()
+      .slice(
+        0,
+        new Date(date).toISOString().lastIndexOf(".")
+      )}","TicketType":1,"IsExclusiveCompartment":false,"PassengerCount":1,"ReturnDate":null,"ServiceType":null,"Channel":1,"AvailableTargetType":null,"Requester":null,"UserId":504604002,"OnlyWithHotel":false,"ForceUpdate":null}`;
+  } else {
+    return `{"From":1,"To":161,"DepartureDate":"${new Date(date)
+      .toISOString()
+      .slice(
+        0,
+        new Date(date).toISOString().lastIndexOf(".")
+      )}","TicketType":1,"IsExclusiveCompartment":false,"PassengerCount":1,"ReturnDate":null,"ServiceType":null,"Channel":1,"AvailableTargetType":null,"Requester":null,"UserId":504604002,"OnlyWithHotel":false,"ForceUpdate":null}`;
+  }
+};
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [times, setTimes] = useState([]);
+  const [details, setDetails] = useState({
+    selectedTime: "",
+    destination: "tehran",
+    date: new Date().toISOString().substring(0, 10),
+  });
+  const [counter, setCounter] = useState(0);
+  const [available, setAvailable] = useState([]);
+  const getTimesHandler = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://ws.alibaba.ir/api/v2/train/available/${btoa(
+          genereteProperText(details?.destination, details?.date)
+        )}`
+      );
+      const {
+        result: { departing },
+      } = data;
+      const fetchedTimes = departing.map((t) =>
+        t.departureDateTime.split("T")[1].slice(0, 5)
+      );
+      setTimes(fetchedTimes);
+    } catch (error) {
+      toast.error(error?.response?.data?.error?.message);
+    }
+  };
+  const soundRef = useRef();
+  const testSoundHandler = () => {
+    soundRef.current.play();
+  };
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  useEffect(() => {
+    let timerId;
+    (async () => {
+      if (counter > 0) {
+        try {
+          timerId = setTimeout(async () => {
+            const { data } = await axios.get(
+              `https://ws.alibaba.ir/api/v2/train/available/${btoa(
+                genereteProperText(details?.destination, details?.date)
+              )}`
+            );
+
+            const {
+              result: { departing },
+            } = data;
+            const availableTickets = departing.filter((t) => t.seat > 0);
+            setAvailable(availableTickets);
+            const selectedTicket = availableTickets.find((t) =>
+              t.departureDateTime.includes(details.selectedTime)
+            );
+            if (selectedTicket) {
+              soundRef.current.play();
+            }
+            setCounter((prev) => prev + 1);
+          }, 20000);
+        } catch (error) {
+          toast.error(error?.response?.data?.error?.message);
+          toast.error(
+            "در صورت بازماندن این صفحه ، پس از آن به صورت خودکار عملیات شکار آغاز خواهد شد"
+          );
+        }
+      }
+    })();
+    return () => {
+      if (timerId) clearTimeout(timerId);
+    };
+  }, [counter]);
+
+  return (
+    <main className="container mx-auto md:w-1/2 md:max-w-lg flex flex-col items-center p-8 gap-6">
+      <div className="flex w-full justify-between items-center">
+        <span>تاریخ</span>
+        <input
+          type="date"
+          className="border border-indigo-500 p-2 rounded-md bg-white outline-none"
+          value={details?.date}
+          onChange={(e) => setDetails({ ...details, date: e.target.value })}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="flex rounded-md overflow-hidden justify-center mx-auto">
+        <div
+          className={`text-center px-7 py-2 ${
+            details?.destination === "tehran"
+              ? " bg-indigo-400 text-white"
+              : "bg-white border-l border-indigo-300"
+          }`}
+          onClick={() => {
+            setDetails({ ...details, destination: "tehran" });
+          }}
         >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          قم به تهران
+        </div>
+        <div
+          className={`text-center px-7 py-2 ${
+            details?.destination === "qom"
+              ? "bg-indigo-400 text-white"
+              : " bg-white border-l border-indigo-300"
+          }`}
+          onClick={() => {
+            setDetails({ ...details, destination: "qom" });
+          }}
         >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
+          تهران به قم
+        </div>
+      </div>
+      <button
+        onClick={getTimesHandler}
+        className="w-full px-7 py-2 bg-indigo-500 text-white rounded-md cursor-pointer disabled:bg-slate-400"
+      >
+        دریافت ساعت ها
+      </button>
+      <button
+        onClick={testSoundHandler}
+        className="w-full px-7 py-2 bg-indigo-500 text-white rounded-md cursor-pointer disabled:bg-slate-400"
+      >
+        تست صدا
+      </button>
+      <div className="flex flex-wrap gap-y-2">
+        {times.map((t) => (
+          <div
+            key={t}
+            onClick={() => {
+              setDetails({ ...details, selectedTime: t });
+            }}
+            className={`text-center px-3 py-2 ${
+              t === details?.selectedTime
+                ? "bg-indigo-400 text-white"
+                : "bg-white "
+            } border-l border-indigo-300`}
           >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+            {t}
+          </div>
+        ))}
+      </div>
+      <button
+        disabled={!details.selectedTime || counter > 0}
+        className="w-full px-7 py-2 bg-indigo-600 text-white rounded-md cursor-pointer disabled:bg-slate-400"
+        onClick={() => setCounter((prev) => prev + 1)}
+      >
+        {counter > 0 ? "در حال شکار" : "آغاز شکار "}{" "}
+      </button>
+      <div className="w-full flex justify-between">
+        <div>تعداد درخواست ها</div>
+        <span>{counter}</span>
+      </div>
+      <h3 className="self-start">بلیط های موجود</h3>
+      <div className="w-full flex flex-col self-start">
+        {available.map((t) => (
+          <div key={t.departureDateTime} className="flex justify-between">
+            <div className="flex gap-x-2">
+              <span>تعداد</span>
+              <span>{t?.seat}</span>
+            </div>
+            <span>{t.departureDateTime.split("T")[1].slice(0, 5)}</span>
+          </div>
+        ))}
+        <audio
+          style={{ display: "none" }}
+          ref={soundRef}
+          controls
+          src="./beep.wav"
         >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          Your browser does not support the
+          <code>audio</code> element.
+        </audio>
       </div>
     </main>
-  )
+  );
 }
